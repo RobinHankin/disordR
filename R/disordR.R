e44f175042a357c098f93042c5c7ee40ec855b7c
Robin Hankin <hankin.robin@gmail.com>
2021-06-09 18:58:35 +1200

    fixes #3

--- R/disordR.R
+++ R/disordR.R
@@ -21,7 +21,9 @@ setClass("disord",
 
 `is.disord` <- function(x){inherits(x,"disord")}
 
-`consistent` <- function(x,y){identical(hash(x),hash(y))}
+allsame <- function(x){length(table(elements(x)))==1}
+
+`consistent` <- function(x,y){identical(hash(x),hash(y)) | allsame(x) | allsame(y)}
 `%~%` <- function(x,y){consistent(x,y)}
 
 
