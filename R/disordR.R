531a488c507bf4efbceca40b69f2ec4f4089f3b6
Robin Hankin <hankin.robin@gmail.com>
2021-06-11 11:27:40 +1200

    names

--- R/disordR.R
+++ R/disordR.R
@@ -46,6 +46,40 @@ setMethod("length","disord",function(x){length(elements(x))})
 setGeneric("length<-")
 setReplaceMethod("length","disord",function(x){stop("cannot set length of a disord object")})
 
+setGeneric("names")
+setMethod("names","disord",
+          function(x){
+            jj <- names(elements(x))
+            if(is.null(jj)){
+              return(NULL)
+            } else {
+              return(disord(jj,h=hash(x)))
+            }
+          } )
+
+setGeneric("names<-")
+setReplaceMethod("names",signature(x="disord",value="disord"),
+                 function(x,value){
+                   stopifnot(consistent(x,value))
+                   jj <- elements(x)
+                   names(jj) <- elements(value)
+                   return(disord(jj,h=hash(x)))
+                 } )
+
+setReplaceMethod("names",signature(x="disord",value="ANY"),
+                 function(x,value){
+                   stopifnot(length(value)==1)
+                   jj <- elements(x)
+                   names(jj) <- elements(value)
+                   return(disord(jj,h=hash(x)))
+                 } )
+                   
+setGeneric("as.character")
+setMethod("as.character",signature=c(x="disord"),function(x){disord(as.character(elements(x)),h=hash(x))})
+  
+                   
+
+
 `disord_arith_disord` <- function(e1,e2){
     stopifnot(consistent(e1,e2))
   switch(.Generic,
@@ -189,6 +223,7 @@ setMethod("[", signature("disord",i="disord",j="missing",drop="ANY"),  # makes t
           })
 
 setMethod("[", signature("disord",i="index",j="ANY",drop="ANY"),function(x,i,j,drop){stop("cannot have two index args")})
+setMethod("[", signature("ANY",i="disord",j="ANY",drop="ANY"),function(x,i,j,drop){"asdfa"})
 
 setReplaceMethod("[",signature(x="disord",i="index",j="missing",value="ANY"),  # a[1:5] <- a[1:5] + 33  = fake
                  function(x,i,j,value){stop("cannot use a regular index to extract, only a disord object")}
