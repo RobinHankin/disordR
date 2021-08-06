af492bb44c79feea26bcbe8e875b4fcb0292fd73
Robin Hankin <hankin.robin@gmail.com>
2021-08-06 13:17:26 +1200

    hex sticker

--- inst/disordR_stickermaker.R
+++ inst/disordR_stickermaker.R
@@ -16,7 +16,7 @@ matrix(nrow=nchar(string)) %>%
 apply(2,paste,collapse="")  -> d
 
 imgurl <- system.file("figures/cat.png", package="hexSticker")
-s <- sticker(subplot=imgurl,package="disordR", p_size=6, s_x=1, s_y=.75, s_width=0,h_fill="#330099",h_color="#000000",
+s <- sticker(subplot=imgurl,package="disordR", p_size=6, s_x=1, s_y=.75, s_width=0,h_fill="#7733FF",h_color="#000000",
              filename="hexcat.png")
 
 s <- s + geom_pkgname(d[0135],size=3.5,x=0.6,y=1.15)
