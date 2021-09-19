4468d02de7782028f899938f8518b98d2928568e
Robin Hankin <hankin.robin@gmail.com>
2021-09-20 10:36:17 +1200

    slight tweaks to color and position

--- inst/disordR_stickermaker.R
+++ inst/disordR_stickermaker.R
@@ -16,17 +16,19 @@ matrix(nrow=nchar(string)) %>%
 apply(2,paste,collapse="")  -> d
 
 imgurl <- system.file("figures/cat.png", package="hexSticker")
-s <- sticker(subplot=imgurl,package="disordR", p_size=6, s_x=1, s_y=.75, s_width=0,h_fill="#7733FF",h_color="#000000",
+s <- sticker(subplot=imgurl,package="disordR", p_size=24, s_x=1, s_y=.75, s_width=0,h_fill="#7733FF",h_color="#000000",
              filename="hexcat.png")
 
-s <- s + geom_pkgname(d[2457],size=3.5,x=0.6,y=1.15)
-s <- s + geom_pkgname(d[0135],size=3.5,x=0.6,y=0.95)
-s <- s + geom_pkgname(d[0121],size=3.5,x=0.6,y=0.75)
-s <- s + geom_pkgname(d[1672],size=3.5,x=0.6,y=0.55)
+jjs <- 10
 
-s <- s + geom_pkgname(d[0087],size=3.5,x=1.4,y=1.15)
-s <- s + geom_pkgname(d[1529],size=3.5,x=1.4,y=0.95)
-s <- s + geom_pkgname(d[2217],size=3.5,x=1.4,y=0.75)
-s <- s + geom_pkgname(d[1156],size=3.5,x=1.4,y=0.55)
+s <- s + geom_pkgname(d[2457],size=jjs, x=0.6,y=1.0)
+s <- s + geom_pkgname(d[0135],size=jjs, x=0.6,y=0.8)
+s <- s + geom_pkgname(d[0121],size=jjs, x=0.6,y=0.6)
+s <- s + geom_pkgname(d[1672],size=jjs, x=0.6,y=0.4)
+
+s <- s + geom_pkgname(d[0087],size=jjs, x=1.4,y=1.0)
+s <- s + geom_pkgname(d[1529],size=jjs, x=1.4,y=0.8)
+s <- s + geom_pkgname(d[2217],size=jjs, x=1.4,y=0.6)
+s <- s + geom_pkgname(d[1156],size=jjs, x=1.4,y=0.4)
 
 save_sticker("disordR.png", s, dpi = 300)
